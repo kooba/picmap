b423d043540bc5427ae200ac3c98ec48c6cfb012
Jakub Borys <jakub.borys@gmail.com>
2014-06-04 22:11:39 +0100

    starting with simple webserver

--- picmap.go
+++ picmap.go
@@ -6,9 +6,20 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"flag"
+	"html/template"
+	"net/http"
 )
 
+var addr = flag.String("addr", ":1718", "http service address")
+
+var templ = template.Must(template.New("qr").Parse(templateStr))
+
+
 func main() {
+	
+	
+
 	fname := "sample1.jpg"
 
 	f, err := os.Open(fname)
@@ -41,4 +52,36 @@ func main() {
             fmt.Println(f.Name())
     }
 
+    flag.Parse()
+    http.Handle("/", http.HandlerFunc(showPics))
+    err = http.ListenAndServe(*addr, nil)
+    if err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+    
 }
+
+func showPics(w http.ResponseWriter, req *http.Request) {
+    templ.Execute(w, req.FormValue("s"))
+}
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
